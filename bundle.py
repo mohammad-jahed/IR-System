337b57a96e58cb37a49132c5147c71f943009f41
mohammad-jahed <mohamed.jahed33@gmail.com>
2023-05-21 21:25:54 +0300

    needs testing and evaluation

--- bundle.py
+++ bundle.py
@@ -4,9 +4,12 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import numpy as  np
-def preprocess_text(text):
+import numpy as np
 
+vectorizer = TfidfVectorizer()
+
+
+def preprocess_text(text):
     tokens = word_tokenize(text)
     stop_words = set(stopwords.words('english'))
     tokens = [token for token in tokens if token.lower() not in stop_words]
@@ -20,9 +23,9 @@ def preprocess_text(text):
 
 
 def represent_data(data):
-    vectorizer = TfidfVectorizer()
     X = vectorizer.fit_transform(data)
     return X
+
 
 # الفهرسة Indexing
 def build_index(data):
@@ -36,46 +39,64 @@ def build_index(data):
                 inverse_index[term].append(i)
     return inverse_index
 
-# Read data
-df = pd.read_csv('path_to_dataset.csv')
-# Data preprocess
-df['processed_text'] = df['text'].apply(preprocess_text)
-# Data representation
-X = represent_data(df['processed_text'])
 
-# Build inverse index
-inverse_index = build_index(df['processed_text'])
+# # Read data
+# df = pd.read_csv('path_to_dataset.csv')
+# # Data preprocess
+# df['processed_text'] = df['text'].apply(preprocess_text)
+# # Data representation
+# X = represent_data(df['processed_text'])
+#
+# # Build inverse index
+# inverse_index = build_index(df['processed_text'])
 
 
-def match_query(query_vector, document_vectors, inverse_index):
-
+def match_query(query_vector, document_vectors, candidate_docs, data, top_k=10):
+    query_vector = vectorizer.transform([query_vector])
     similarities = cosine_similarity(query_vector, document_vectors).flatten()
 
-    # ترتيب النتائج بناءً على قيم التشابه
-    ranked_indices = np.argsort(similarities)[::-1]
-    ranked_documents = inverse_index.iloc[ranked_indices]
+    # Rank the documents
+    sorted_indices = np.argsort(similarities)[::-1]
+    # print(sorted_indices)
+    # print(data)
+    ranked_documents = [data.iloc[i] for i in sorted_indices if i in candidate_docs]
 
-    return ranked_documents
+    # Return the top-ranked documents
+    search_results = ranked_documents[:top_k]
+    return search_results
 
 
-query = "information retrieval"
-query_vector = represent_data([preprocess_text(query)])[0]
-
-results = match_query(query_vector, X, inverse_index)
-print(results)
-# # استعلام المستخدم
-# def search(query, inverse_index, data):
-#     query = preprocess_text(query)
-#     query_terms = query.split()
+# query = "information retrieval"
+# query_vector = represent_data([preprocess_text(query)])[0]
 #
-#     # استرجاع المستندات ذات الصلة
-#     relevant_docs = set()
-#     for term in query_terms:
-#         if term in inverse_index:
-#             relevant_docs.update(inverse_index[term])
-#
-#     # إرجاع المستندات المطابقة
-#     return data.iloc[list(relevant_docs)]
+# results = match_query(query_vector, X, inverse_index)
+# print(results)
+
+
+def get_candidate_docs(query, inverse_index, data):
+    # query = preprocess_text(query)
+    query_terms = query.split()
+    relevant_docs = set()
+    for term in query_terms:
+        if term in inverse_index:
+            relevant_docs.update(inverse_index[term])
+    return relevant_docs
+
+
+def process_queries(queries, document_vectors, inverse_index, df):
+    match_result = {}
+    for index, query in queries.iterrows():
+        processed_query = preprocess_text(query['text'])
+        candidate_docs = get_candidate_docs(processed_query, inverse_index, df)
+        query_results = match_query(processed_query, document_vectors, candidate_docs, df, top_k=5)
+        match_result[query['query_id']] = [doc['doc_id'] for doc in query_results]
+        if len(query_results) > 0:
+            print(f'query: {query}\nresult: {query_results[1]}')
+        else:
+            print(f'query: {query}\n no documents found')
+
+        print('===============================================')
+    return match_result
 #
 # # استخدام النظام
 # query = "information retrieval"
--- main.py
+++ main.py
@@ -3,22 +3,36 @@ import nltk
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import pandas as pd
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+
 import IR1
+import bundle
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    df = IR1.read_df(path='dataset/docs.csv')
-    tokenized_df = IR1.tokenize_df(df.head(10))
-    print(f'tokenized_df: {tokenized_df}')
-    clear_data = IR1.clean_data(tokenized_df)
-    print(f'clear_data: {clear_data}')
-    merged_text = [' '.join(row) for row in clear_data]
-    tfidf_vectors, features = IR1.tfidf_vector(merged_text)
-    print(f'tfidf_vectors {tfidf_vectors}\nfeatures: {features}')
+# if __name__ == '__main__':
+#     df = IR1.read_df(path='dataset/docs.csv')
+#     tokenized_df = IR1.tokenize_df(df.head(10))
+#     print(f'tokenized_df: {tokenized_df}')
+#     clear_data = IR1.clean_data(tokenized_df)
+#     print(f'clear_data: {clear_data}')
+#     merged_text = [' '.join(row) for row in clear_data]
+#     tfidf_vectors, features = IR1.tfidf_vector(merged_text)
+#     print(f'tfidf_vectors {tfidf_vectors}\nfeatures: {features}')
 
+if __name__ == '__main__':
+    df = pd.read_csv('dataset/docs.csv')  # read data
+    df = df.head(100)
+    df['processed_text'] = df['text'].apply(bundle.preprocess_text)  # Data Preprocessing
+    document_vectors = bundle.represent_data(df['processed_text'])  # Data Representation
+    inverse_index = bundle.build_index(df['processed_text'])  # Indexing
+    # match query
+    queries = pd.read_csv('dataset/queries.csv').head(10)  # Read queries
+    result_matches = bundle.process_queries(queries, document_vectors, inverse_index, df)  # mathing and ranking queries
+    print(result_matches)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
