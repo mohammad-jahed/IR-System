ea4fe16935ceff6d39345e3f12459a093caa0a79
mohammad-jahed <mohamed.jahed33@gmail.com>
2023-06-09 18:02:31 +0300

    Some Enhancement

--- main.py
+++ main.py
@@ -16,108 +16,17 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-import IR1
-import bundle
-import test
 import IR
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:4200'])
-
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     df = IR1.read_df(path='dataset/docs.csv')
-#     tokenized_df = IR1.tokenize_df(df.head(10))
-#     print(f'tokenized_df: {tokenized_df}')
-#     clear_data = IR1.clean_data(tokenized_df)
-#     print(f'clear_data: {clear_data}')
-#     merged_text = [' '.join(row) for row in clear_data]
-#     tfidf_vectors, features = IR1.tfidf_vector(merged_text)
-#     print(f'tfidf_vectors {tfidf_vectors}\nfeatures: {features}')
-
-# if __name__ == '__main__':
-#     df = pd.read_csv('dataset/docs.csv')  # read data
-#     #df = df.head(100)
-#     df['processed_text'] = df['text'].apply(bundle.preprocess_text)  # Data Preprocessing
-#     document_vectors = bundle.represent_data(df['processed_text'])  # Data Representation
-#     inverse_index = bundle.build_index(df['processed_text'])  # Indexing
-#     # match query
-#     queries = pd.read_csv('dataset/queries.csv')#.head(10)  # Read queries
-#     result_matches = bundle.process_queries(queries, document_vectors, inverse_index, df)  # mathing and ranking queries
-#     print(result_matches)
-#     #######################################################################################
-#     qrels = pd.read_csv('dataset/qrels.csv')
-#     relevant_docs = qrels.loc[qrels['query_id'] == queries['query_id'][0], 'doc_id']
-#     print(relevant_docs)
-#
-#     intersection = np.intersect1d(relevant_docs, result_matches[queries['query_id'][0]])
-#     print(intersection)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# CORS(app, origins=['http://localhost:4200'])
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
-#     df = pd.read_csv("dataset/docs.csv")
-#
-#     # df = df.head(100000)
-#
-#     df['processed_text'] = df['text'].apply(test.preprocess_text)
-#
-#     document_vectors = test.represent_text(df['processed_text'])
-#
-#     inverse_index = test.build_index(df['processed_text'])
-#     #
-#     # with open('vectorized.pickle', 'wb') as file:
-#     #     pickle.dump(document_vectors, file)
-#     # with open('inverse_index.pickle', 'wb') as file:
-#     #     pickle.dump(inverse_index, file)
-#
-#     queries = pd.read_csv('dataset/queries.csv')#.head(5)  # Read queries
-#
-#     # with open('document_vectors.pickle', 'rb') as f:
-#     #     document_vectors1 = pickle.load(f)
-#     # with open('inverse_index.pickle', 'rb') as f:
-#     #     inverse_index1 = pickle.load(f)
-#
-#     result_matches = test.process_queries(queries, document_vectors, inverse_index, df)
-#     print(result_matches)
-#
-#
-#     @app.route('/process_queries', methods=['POST'])
-#     def process_queries():
-#         query_data = request.get_json()
-#         queries_api = query_data['queries']
-#         # document_vectors = query_data['document_vectors']
-#         # inverse_index = query_data['inverse_index']
-#         # df = query_data['df']
-#
-#         match_result = {}
-#         for index, query in queries_api.iterrows():
-#             processed_query = test.preprocess_text(queries_api)
-#             candidate_docs = test.get_candidate_docs(processed_query, inverse_index)
-#             query_results = test.match_query(processed_query, document_vectors, candidate_docs, df)
-#             match_result[query['query_id']] = [doc['doc_id'] for doc in query_results]
-#
-#         return jsonify(match_result)
-#
-#
-#     qrels = pd.read_csv('dataset/qrels.csv')
-#
-#     relevant_docs = qrels.loc[qrels['query_id'] == queries['query_id'][0], 'doc_id']
-#     print(relevant_docs)
-#
-#     #Compare
-#     intersection = np.intersect1d(relevant_docs, result_matches[queries['query_id'][0]])
-#     print(intersection)
-#
-#     evaluation_results = {}
-#     test.evaluation(queries, result_matches, qrels, evaluation_results)
-#     print(evaluation_results)
 @app.route('/')
 def hello_world():
     return "Hello, World!"
+
 
 if __name__ == '__main__':
 
@@ -158,7 +67,7 @@ if __name__ == '__main__':
         inverse_index_stored2 = pickle.load(f)
 
     queries1 = pd.read_csv('dataset/queries.csv')  # Read queries
-    queries2 = pd.read_csv('dataset2/queries.csv') # Read queries
+    queries2 = pd.read_csv('dataset2/queries.csv')  # Read queries
 
     qrels1 = pd.read_csv('dataset/qrels.csv')
     qrels2 = pd.read_csv('dataset2/qrels.csv')
@@ -183,18 +92,17 @@ if __name__ == '__main__':
 
     evaluation_results1 = {}
     avg_f1, avg_pre1, avg_pre_at_k1, avg_mrr1 = IR.evaluation(queries1, result_matches1, qrels1, evaluation_results1)
-    print("FMeasure Average = ",avg_f1)
-    print("Mean Average Precision = ",avg_pre1) 
-    print("Precesion Average @10 = ",avg_pre_at_k1)
-    print("Mean Reciprocal Rank = ",avg_mrr1)
+    print("FMeasure Average = ", avg_f1)
+    print("Mean Average Precision = ", avg_pre1)
+    print("Precesion Average @10 = ", avg_pre_at_k1)
+    print("Mean Reciprocal Rank = ", avg_mrr1)
 
     evaluation_results2 = {}
     avg_f2, avg_pre2, avg_pre_at_k2, avg_mrr2 = IR.evaluation(queries2, result_matches2, qrels2, evaluation_results2)
-    print("FMeasure Average = ",avg_f2)
-    print("Mean Average Precision = ",avg_pre2) 
-    print("Precesion Average @10 = ",avg_pre_at_k2)
-    print("Mean Reciprocal Rank = ",avg_mrr2)
-
+    print("FMeasure Average = ", avg_f2)
+    print("Mean Average Precision = ", avg_pre2)
+    print("Precesion Average @10 = ", avg_pre_at_k2)
+    print("Mean Reciprocal Rank = ", avg_mrr2)
 
 
     @app.route('/process_queries1', methods=['POST'])
@@ -228,7 +136,5 @@ if __name__ == '__main__':
             arr[key] = i['text']
         return jsonify(arr)
 
+
     app.run(debug=True)
-
-
-
